53c0ba28298d355e20b85d057303c0103b1ecd57
Carlos Andres Coronado <carloscoronado098@gmail.com>
2021-11-27 15:25:38 -0400

    <SSM-FDisplayOrderInfo><Completed visualization with responsive environment, maybe missing some styling>

--- ChachasFrontend/src/app/shared-modules/sales/display-order-info/display-order-info.component.ts
+++ ChachasFrontend/src/app/shared-modules/sales/display-order-info/display-order-info.component.ts
@@ -11,14 +11,15 @@ export class DisplayOrderInfoComponent implements OnInit {
   productComponents: editComponent[] = []
 
   productosPrueba: Product[] = [
-    {name: "chacha de charque", quantity: 10},
-    {name: "chacha de queso", quantity: 2},
-    {name: "chacha de carne", quantity: 5}
+    {name: "Chacha de Charque", quantity: 10, price: 12, image: "https://img.freepik.com/foto-gratis/empanadas-espanolas-tipicas-blanco_123827-9710.jpg?size=626&ext=jpg", description: "es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen."},
+    {name: "Chacha de Queso", quantity: 2, price: 9, image: "https://img.freepik.com/foto-gratis/empanadas-espanolas-tipicas-blanco_123827-9710.jpg?size=626&ext=jpg", description: "es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos."},
+    {name: "Chacha de Carne", quantity: 5, price: 10, image: "https://img.freepik.com/foto-gratis/empanadas-espanolas-tipicas-blanco_123827-9710.jpg?size=626&ext=jpg", description: "es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta)"}
   ];
 
   constructor() { 
     for (let producto of this.productosPrueba){
-      let aux: editComponent = {product: producto, edit: false}
+      let total = producto.price * producto.quantity
+      let aux: editComponent = {product: producto, edit: false, totalPrice: total}
       this.productComponents.push(aux)
     }
   }
@@ -26,25 +27,41 @@ export class DisplayOrderInfoComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  editProductQuantity(comp: editComponent){
+  editProductQuantity(comp: editComponent) {
     comp.edit = true
     console.log("The element is now: " + comp.edit)
   }
 
-  acceptEditOfProductQuantity(comp: editComponent, newQuantity: string){
+  acceptEditOfProductQuantity(comp: editComponent, newQuantity: string) {
     let aux = Number(newQuantity)
+    let newTotal = aux * comp.product.price
+    
     comp.product.quantity = aux
+    comp.totalPrice = newTotal
+
     comp.edit = false
     console.log("The element is now: " + comp.edit)
+  }
+
+  acceptProductChoice() {
+
+  }
+
+  keepChoosing() {
+
   }
 }
 
 interface Product {
   name: string,
-  quantity: number 
+  quantity: number,
+  price: number,
+  image: string,
+  description: string
  }
  
  interface editComponent {
    product: Product,
-   edit: boolean
+   edit: boolean,
+   totalPrice: number
  }
