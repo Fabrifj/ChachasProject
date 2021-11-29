eeb0aa532b85a420273f6c975c33b17aefc66684
Stephany78315 <stephanyduran78315@gmail.com>
2021-11-29 11:49:30 -0400

    CRUD of Menu

--- ChachasBackend/index.js
+++ ChachasBackend/index.js
@@ -6,6 +6,7 @@ app.use(express.json());
 app.use(cors());
 
 const fnSubsidiary = require("./subsidiary");
+const fnMenu = require("./menu");
 /*===================================
           CRUD SUBSIDIARY
 ===================================*/
@@ -21,5 +22,44 @@ app.get("/api/subsidiary", async (req, res) => {
     const respuesta = await fnSubsidiary.getSubsidiaries();
     res.send(respuesta);
   });
+
+
+/*===================================
+          CRUD MENU
+===================================*/
+// Create Menu
+app.post("/api/menu", async (req, res) => {
+  var body = req.body;
+  const respuesta = await fnMenu.createMenu(body);
+  res.send(respuesta);
+});
+
+// Get Menus
+app.get("/api/menu", async (req, res) => {
+  const respuesta = await fnMenu.getMenus();
+  res.send(respuesta);
+});
+
+//Get Menu by Name
+app.get("/api/menu/getMenuName", async (req, res) => {
+  var body = req.body;
+  var respuesta = await fnMenu.getMenuName(body);
+  res.send(respuesta);
+});
+
+//Update Menu
+app.put("/api/menu/:id", async (req, res) => {
+  var menid = req.params.id;
+  var men = req.body;
+  const respuesta = await fnMenu.updateMenu(menid,men);
+  res.send(respuesta);
+});
+
+//Delete Menu by id
+app.delete("/api/menu/:id", async (req, res) => {
+  var men = req.params.id;
+  const resp = await fnMenu.deleteMenu(men);
+  res.send(resp);
+});
 
 app.listen(4000, () => console.log("Up and Running on 4000"));
