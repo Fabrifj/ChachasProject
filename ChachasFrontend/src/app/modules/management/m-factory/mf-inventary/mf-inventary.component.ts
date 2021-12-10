834503a5c1f1d8b7aae7a2c92231ce3075b9a4f4
Andrea Sarmiento <anbesamo@gmail.com>
2021-12-09 22:14:33 -0400

    FMF-00 cambios v0

---
+++ ChachasFrontend/src/app/modules/management/m-factory/mf-inventary/mf-inventary.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mf-inventary',
+  templateUrl: './mf-inventary.component.html',
+  styleUrls: ['./mf-inventary.component.css']
+})
+export class MfInventaryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
