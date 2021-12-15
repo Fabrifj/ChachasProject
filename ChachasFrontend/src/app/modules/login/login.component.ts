2dd857e7441d938b692e4a7e8e745fefb6690af0
Fabrifj <fabrifj@gmail.com>
2021-12-15 14:28:37 -0400

    clear ?

--- ChachasFrontend/src/app/modules/login/login.component.ts
+++ ChachasFrontend/src/app/modules/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AppHttpService } from 'src/app/core-modules/app-http.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup ;
+  loading = false;
+  submitted = false;
+  returnUrl: string ="";
+  
+  constructor(
+    private formBuilder:FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private appHttpService: AppHttpService
+    ) {
+    this.form = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+   }
 
-  ngOnInit(): void {
+ 
+  ngOnInit() {
+    
+    // get return url from route parameters or default to '/'
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+  }
+
+  // convenience getter for easy access to form fields
+  get f() { return this.form.controls; }
+
+  onSubmit() {
+      this.submitted = true;
+
+
+      // stop here if form is invalid
+      if (this.form.invalid) {
+          return;
+      }
+
+      this.loading = true;
+      console.log(this.f['username'].value+"  "+this.f['password'].value)
+      this.appHttpService.login({user:this.f['username'].value, pass:this.f['password'].value})
+          .subscribe(
+              data => {
+                console.log(data);
+                this.router.navigate([this.returnUrl]);
+              });
   }
 
 }
---
+++ ChachasFrontend/src/app/modules/login/login.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor() { }
+
+  user:string ="";
+  state:string="";
+  accountType:string="";
+
+  updatedUser(){
+    this.user;
+    this.accountType;
+    this.state = "Login";
+  }
+  getUser(){
+    return this.user;
+  }
+  getAccountType(){
+    return this.accountType; 
+  }
+}
